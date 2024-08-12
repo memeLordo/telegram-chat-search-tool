4f310434376690eda69cdd5469485130c20fad5d
memeLordo <simaforest@BAD-BOY>
2024-08-12 15:22:02 +0300

    Add setter and getter

--- main.py
+++ main.py
@@ -4,14 +4,23 @@ from dotenv import dotenv_values, set_key
 def get_env_keys():
     try:
         env_config = dotenv_values(".env.script")
-        api_id = env_config["BOT_ID"]
-        api_hash = env_config["BOT_HASH"]
+        api_id = env_config["API_ID"]
+        api_hash = env_config["API_HASH"]
+        return (api_id, api_hash)
     except KeyError:
-        api_id = input("API_ID: ")
+        return set_env_keys()
+
+
+def set_env_keys():
+    try:
+        api_id = int(input("API_ID: "))
         api_hash = input("API_HASH: ")
-        set_key(".env.script", "API_ID", api_id)
+        set_key(".env.script", "API_ID", str(api_id))
         set_key(".env.script", "API_HASH", api_hash)
-    return (api_id, api_hash)
+        return (api_id, api_hash)
+    except ValueError:
+        print("IP должно быть числом!")
+        return set_env_keys()
 
 
 def main():
