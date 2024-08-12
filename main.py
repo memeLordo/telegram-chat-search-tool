c20e095c8d8d3374956a31da24f78d1ebc36fd91
memeLordo <simaforest@BAD-BOY>
2024-08-12 15:05:45 +0300

    Added missing KeyError handling

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from dotenv import dotenv_values
+from dotenv import dotenv_values, set_key
+
+
+def get_env_keys():
+    try:
+        env_config = dotenv_values(".env.script")
+        api_id = env_config["BOT_ID"]
+        api_hash = env_config["BOT_HASH"]
+    except KeyError:
+        api_id = input("API_ID: ")
+        api_hash = input("API_HASH: ")
+        set_key(".env.script", "API_ID", api_id)
+        set_key(".env.script", "API_HASH", api_hash)
+    return (api_id, api_hash)
+
+
 def main():
-    env_config = dotenv_values(".env.script")
-    api_id = env_config["BOT_ID"]
-    api_hash = env_config["BOT_HASH"]
+    api_id, api_hash = get_env_keys()
     print(api_id, api_hash, end="\n")
+
+
 if __name__ == "__main__":
     main()
