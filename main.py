1a7b714ce28ceaeaf38c0a5f537c8941aa9aded0
memeLordo <simaforest@BAD-BOY>
2024-08-19 18:14:25 +0300

    Add [] inside filename

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ def mkdir(dirname: str):
 def save_to_txt(text: str):
     global index
     dir = mkdir("./requests")
-    filename = f"{dir}/request{index}.txt"
+    filename = f"{dir}/request[{index}].txt"
     while os.path.exists(filename):
         index += 1
-        filename = f"{dir}/request{index}.txt"
+        filename = f"{dir}/request[{index}].txt"
     with open(filename, "w+") as file:
         file.write(text)
     index += 1
