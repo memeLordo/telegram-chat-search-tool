ce8aa74b7dc5d99b730cf4976b7538bd5c8874e7
memeLordo <simaforest@BAD-BOY>
2024-08-19 18:01:46 +0300

    Add dir checking

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ def save_to_txt(text: str):
     global index
     dir = mkdir("./requests")
     filename = f"{dir}/request{index}.txt"
+    while os.path.exists(filename):
+        index += 1
+        filename = f"{dir}/request{index}.txt"
     with open(filename, "w+") as file:
         file.write(text)
     index += 1
