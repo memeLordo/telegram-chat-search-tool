32e07f1a9073c6d8c53862190515de482bc2dae7
memeLordo <simaforest@BAD-BOY>
2024-08-27 18:32:33 +0300

    Add callable param for error_handler

--- search/local/errors.py
+++ search/local/errors.py
@@ -1,11 +1,12 @@
-def error_handler(errors=(Exception,), err_message=" "):
+def error_handler(errors=(Exception,), err_message=" ", callable=True):
     def wrapper(func):
         def wrapped(*args, **kwargs):
             try:
                 return func(*args, **kwargs)
-
-            except errors as e:
-                return func(*args, **kwargs)
+            except errors:
+                print(err_message)
+                if callable:
+                    return func(*args, **kwargs)
 
         return wrapped
 
