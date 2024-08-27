ec10b2c8bc3b6959787b206c7ba4f97c64f26cbd
memeLordo <simaforest@BAD-BOY>
2024-08-27 17:16:52 +0300

    Update load into class

--- search/local/loading.py
+++ search/local/loading.py
@@ -1,4 +1,21 @@
 # TODO: Make Loading class
-symbols = "-" * 50
-loading = f"Loading: [{symbols}]"
-backtrack = "\b" * len(loading)
+
+
+class Load:
+    class Bar:
+        symbol = "-"
+        length = 50
+
+        def __init__(self, symbol=symbol, length=length) -> None:
+            self.symbol = symbol
+            self.length = length
+
+        def __str__(self) -> str:
+            return self.symbol * self.length
+
+    bar = Bar()
+    message = f"Loading: [{bar}]"
+    backtrack = "\b" * len(message)
+
+
+print(Load.bar)
