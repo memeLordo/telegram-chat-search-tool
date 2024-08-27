7bfbd68a9d55a7d6336aeb98cf0967af18c256d9
memeLordo <simaforest@BAD-BOY>
2024-08-27 18:19:54 +0300

    Add error wrapper

---
+++ search/local/errors.py
@@ -0,0 +1,12 @@
+def return_error_handler(errors=(Exception,), err_message=" "):
+    def wrapper(func):
+        def wrapped(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+
+            except errors as e:
+                return func(*args, **kwargs)
+
+        return wrapped
+
+    return wrapper
