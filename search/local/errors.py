0787436daab4ed91f0bdd13de443adb6490e654b
memeLordo <simaforest@BAD-BOY>
2024-08-27 18:59:14 +0300

    Add finaly to error_handler

--- search/local/errors.py
+++ search/local/errors.py
@@ -5,6 +5,7 @@ def error_handler(errors=(Exception,), err_message="Unhandled error!", callable=
                 return func(*args, **kwargs)
             except errors:
                 print(err_message)
+            finally:
                 if callable:
                     return func(*args, **kwargs)
 
