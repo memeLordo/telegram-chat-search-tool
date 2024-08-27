0e884c5d80a5da3e6fb7585fc67df550f8f561b0
memeLordo <simaforest@BAD-BOY>
2024-08-27 17:48:17 +0300

    Update __file__ in Env

--- search/local/config.py
+++ search/local/config.py
@@ -2,6 +2,8 @@ from dotenv import dotenv_values, set_key
 
 
 class Env:
+    __file__ = ".env.config"
+
     @classmethod
     def get_mode_from(cls, env_config: dict[str, str | None]):
         try:
@@ -9,15 +11,15 @@ class Env:
             if str(cls.mode).lower() not in ("title", "link"):
                 raise KeyError
         except KeyError:
-            _, _, cls.mode = set_key(".env.config", "MODE", "link")
+            _, _, cls.mode = set_key(cls.__file__, "MODE", "link")
 
     @classmethod
     def set_keys(cls) -> tuple[int, str]:
         try:
             cls.api_id = int(input("API_ID: "))
             cls.api_hash = input("API_HASH: ")
-            set_key(".env.config", "API_ID", str(cls.api_id))
-            set_key(".env.config", "API_HASH", cls.api_hash)
+            set_key(cls.__file__, "API_ID", str(cls.api_id))
+            set_key(cls.__file__, "API_HASH", cls.api_hash)
             return (cls.api_id, cls.api_hash)
         except ValueError:
             print("IP должно быть числом!")
