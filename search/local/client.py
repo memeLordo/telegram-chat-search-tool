d2346ef3ea1047716302e2fba2ed5bec2c2fec1c
memeLordo <simaforest@BAD-BOY>
2024-09-02 15:49:32 +0300

    client: Create Env params

--- search/local/client.py
+++ search/local/client.py
@@ -3,5 +3,6 @@ from telethon.sync import TelegramClient
 from .config import Env
 from .file import File
 
+Env.create()
 session_dir = File.mkdir("./sessions")
 client = TelegramClient(f"{session_dir}/client", Env.api_id, Env.api_hash)
